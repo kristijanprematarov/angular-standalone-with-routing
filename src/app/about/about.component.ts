9a603650dedab4c717bb8c71342924c0aa239cf4
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2023-01-09 22:18:44 +0100

    init

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './about.component.html'
+})
+export class AboutComponent {}
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+import {WelcomeComponent} from './welcome/welcome.component';
+import {RouterModule} from "@angular/router";
+
+@Component({
+    standalone: true,
+    imports: [
+        RouterModule,
+        WelcomeComponent
+    ],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+}
